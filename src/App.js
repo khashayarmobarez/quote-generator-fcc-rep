87aa4ffe6b52dc04ee132313649e54ab1d71a739
khashayar <khashayarmobarezbusiness@gmail.com>
2022-10-09 18:30:47 +0330

    the finish touch of the project except the animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import QuoteBox from "./components/QuoteBox";
+import styles from "./app.module.css"
+import { colorsCode } from './components/ColorData';
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Color: "",
+    }
+  }
+
+  colors = colorsCode;
+
+  componentDidMount() {
+    this.setState({
+      Color: this.colors[Math.floor(Math.random() * this.colors.length)]
+    })
+  }
+
+  colorHandler = (colors) => {
+    this.setState({
+      Color: this.colors[Math.floor(Math.random() * this.colors.length)]
+    }) 
+  }
+
+  
+
+  render() {
+    const { Color } = this.state;
+    return (
+      <div className={styles.app} style={{background: Color}}>
+  
+          <QuoteBox colorHandler={this.colorHandler} color={Color} />
+          
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/QuoteBox.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import styles from "./QuoteBox.module.css"
+
+class QuoteBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes: [],
+            choosenQ: [],
+            Quote:"Dedication makes dreams come true.",
+            author:"kobe bryant",
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://type.fit/api/quotes')
+            .then(response => response.json())
+            .then(response =>  this.setState({
+                quotes: response,
+                choosenQ: response[Math.floor(Math.random() * response.length)]
+            }))
+    }
+
+
+    clickHandler = () => {
+                this.setState({
+                choosenQ: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)],
+                Quote: this.state.choosenQ.text,
+                author: this.state.choosenQ.author
+            })
+    }
+
+    tweetHandler = () => {
+        const {Quote,author} = this.state;
+        window.open(`https://www.twitter.com/intent/tweet?text=${'"' + Quote+ '"  ' +author}`, "_blank");
+    }
+
+    render() {
+        const {colorHandler, color} = this.props;
+        return (
+            <div className={styles.quoteBox}>
+                <div className={styles.container}>
+
+                    
+                        <h1 className={styles.theQuote} style={{color: color}}>"{this.state.Quote}</h1>
+                        <p className={styles.author} style={{color: color}}>{this.state.author}</p>
+                    
+                    
+                
+
+                <div className={styles.buttonContainer}>
+                    <button className={styles.quoteButton} onClick={() => {colorHandler();this.clickHandler();}} style={{background: color}}>new quote</button>
+                    <button className={styles.tweetButton} onClick={this.tweetHandler} style={{background: color}}>
+                      <i className="fa-brands fa-twitter"></i>   
+                    </button>
+
+                </div>
+              </div>
+             </div>
+        );
+    }
+}
+
+export default QuoteBox;
